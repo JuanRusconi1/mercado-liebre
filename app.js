6d7cdea6583b54d2f1261cb0c86cd8fe541e3146
Juan Rusconi <juancitorusconi@hotmail.com>
2022-09-20 21:55:43 -0300

    Subiendo el proyecto

---
+++ app.js
@@ -0,0 +1,29 @@
+const { Console } = require("console");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+app.post("/register", (req, res) => {
+    
+    res.redirect("/");
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+app.post("/login", (req, res) => {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, () => 
+    console.log("Servidor corriendo en el puerto 3000"))
+
